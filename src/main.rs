6a40778b73a720fb733774d9ed5ec71a7055b5d4
Daniel Kiss <littlesnorrboy@gmail.com>
2025-06-05 13:00:26 +0200

    init

---
+++ src/main.rs
@@ -0,0 +1,63 @@
+use clap::Parser;
+use clap_derive::{Parser, Subcommand};
+
+#[derive(Debug, Subcommand)]
+enum Cmd {
+    Memcpy {
+        /// buffer size in bytes
+        #[arg(short, long, default_value_t = 1048576)]
+        size: usize,
+        /// number of threads to perform the copy on
+        #[arg(short, long, default_value_t = std::thread::available_parallelism().map(|x|x.get()).unwrap_or(1))]
+        threads: usize,
+    },
+}
+
+#[derive(Debug, Parser)]
+struct Cli {
+    #[command(subcommand)]
+    command: Cmd,
+}
+
+fn memcpy_test(size: usize, threads: usize) {
+    let mut src = Vec::<u8>::with_capacity(size);
+    let mut dst = Vec::<u8>::with_capacity(size);
+
+    // touch all allocated pages
+    src.resize(size, 0xBE);
+    dst.resize(size, 0xEF);
+
+    let start;
+    let end;
+    if threads <= 1 {
+        start = std::time::Instant::now();
+        dst.copy_from_slice(src.as_slice());
+        end = std::time::Instant::now();
+    } else {
+        let num_threads = threads;
+
+        start = std::time::Instant::now();
+        std::thread::scope(|s| {
+            let mut threads = Vec::with_capacity(threads);
+            let chunk_size = (size + 1) / num_threads;
+            for (src, dst) in src.chunks(chunk_size).zip(dst.chunks_mut(chunk_size)) {
+                threads.push(s.spawn(|| {
+                    dst.copy_from_slice(src);
+                }));
+            }
+        });
+        end = std::time::Instant::now();
+    }
+
+    let dur = end - start;
+    println!("memcpy test of {size} bytes on {threads} threads duration: {dur:?}");
+    println!("throughput: {}", size as f64 / dur.as_secs_f64())
+}
+
+fn main() {
+    let args = Cli::parse();
+
+    match args.command {
+        Cmd::Memcpy { size, threads } => memcpy_test(size, threads),
+    }
+}
